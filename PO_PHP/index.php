6d8c36941f65c868c9e5da952bd01be045e63415
Liekehinze <204282@stmichaelcollege.nl>
2025-11-20 11:11:12 +0100

    Add files via upload
    ik had gisteravond de opmaak beter geemaakt en alvast een home knop toegevoegd. Vandaag zijn Selma en ik de zoekbalk gaan maken (vooral Selma) en die is helemaal top. Nu stuur ik dit naar selma en gaan we (voor de rest van de les) veerder op haar laptop en de inlogpagina maken

--- PO_PHP/index.php
+++ PO_PHP/index.php
@@ -1,8 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="php.css">
+        <title> BrainBoost| Quiz</title> <!--zorgt ervoor dat dit komt te staan bij de tabblad LH SI-->
+        <!--icoon te zien links bij tabblad LH (w3schools,2024)-->
+        <link rel="shortcut icon" href="quizlogo.png" type="image/x-icon">
+
     </head>
     <body>
+        <header><img src= "brainboost_foto.png"><br>
+            <br>
+            <a href="index.php">HOME</a>
+           <input type="text" placeholder="zoeken">
+        </header>
+         
+        
+
         <?php
 
         ?>
