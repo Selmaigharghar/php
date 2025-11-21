dbba49fb1e00b5ad47df828369cbc4077cba0d09
Liekehinze <204282@stmichaelcollege.nl>
2025-11-21 23:17:13 +0100

    Add files via upload
    ik heb een nieuwe knop gemaakt namelijk voor het niveau. Vorig PO van javascript had ik het geleerd over prompt en vond dat handig hiervoor te gebruiken. Ik wist niet hoe het zou door verwijzen naar php en had dit gevonden "opslaan.php?niveau=". dus dat heb ik gbruikt en daardoor wordt de input van de gebruiker opgeslagen. verder ben ik ook met de php code begonen, maar die doet het niet helemaal, dus ik ga later even kijken hoe dat komt want nu ben ik moe

--- PO_PHP/index.php
+++ PO_PHP/index.php
@@ -10,14 +10,42 @@
     <body>
         <header><img src= "brainboost_foto.png"><br>
             <br>
-            <a href="index.php">HOME</a>
+            <button><a href="index.php">HOME</a></button>
            <input type="text" placeholder="zoeken">
+           <button onclick="vraagNiveau()">Niveau invullen</button>
+
+<script>
+function vraagNiveau() {
+    let niveau = prompt("Welk niveau doe je? (kies uit havo, vwo)");
+
+    // als er niks wordt ingevuld moet het opnieuw gevraagd worden
+    while (!niveau) {
+        niveau = prompt("Vul een geldig niveau in (kies uit havo, vwo):");
+    }
+
+    // stuurt niveau naar PHP opslaan
+    window.location.href = "opslaan.php?niveau=" + niveau; //vragen hoe we ervoor zorgen dat niet elk niveau kan
+}
+</script>
+
         </header>
-         
-        
+<?php
+session_start();
 
-        <?php
-
-        ?>
+if (isset($_GET['niveau'])) {
+    // strtolower zet de gebruikersinput om naar kleine letters 
+    $niveau = strtolower($_GET['niveau']); 
+    
+    // alleen als havo of vwo zijn ingevuld moet de functie verder gaan
+    if ($niveau == "havo" || $niveau == "vwo") {
+        $_SESSION['niveau'] = $niveau;
+        echo "Niveau opgeslagen: " . $_SESSION['niveau'];
+    } else {
+        echo "Ongeldig niveau. Alleen 'havo' of 'vwo' is toegestaan.";
+    }
+} else {
+    echo "Geen niveau ontvangen."; // ik ga morgen uitzoeken waaorm het niet werkt
+}
+?>
     </body>
 </html>
